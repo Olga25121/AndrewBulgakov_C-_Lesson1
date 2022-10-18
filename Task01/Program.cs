c3a03d402dfa4002542371876feb1b0e932bb5db
Olga25121 <olgajensen@mail.ru>
2022-10-19 00:00:00 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+Console.WriteLine("Да, первое число является квадратом второго");
+}
+else
+{
+Console.WriteLine("Нет, первое число не является квадратом второго");
+}
