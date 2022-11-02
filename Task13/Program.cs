a092e08c5ac34cf396f1022b1f1806e2d83fe324
Olga25121 <olgajensen@mail.ru>
2022-11-03 00:36:05 +0300

    Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,21 +6,43 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Console.WriteLine("Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int a = number;
+
+// if (a >= 100)
+// {
+//     while (a > 999)
+//     {
+//         a = a / 10;
+//     }
+//     int thirdDigit = a % 10;
+//     Console.WriteLine($"Третья цифра числа = {thirdDigit}");
+// }
+// else
+// {
+//     Console.WriteLine("третьей цифры нет");
+// }
+
+//метод
 Console.WriteLine("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
-
 int a = number;
 
 if (a >= 100)
 {
-    while (a > 999)
+    int ThirdDigNum(int num)
     {
-        a = a / 10;
+        while (a > 999)
+        {
+            a = a / 10;
+        }
+
+        int thirdDigit = a % 10;
+        return thirdDigit;
     }
-    int thirdDigit = a % 10;
-    Console.WriteLine($"Третья цифра числа = {thirdDigit}");
+    int result = ThirdDigNum(number);
+    Console.WriteLine($"Третья цифра числа => {result}");
 }
-else
-{
-    Console.WriteLine("третьей цифры нет");
-}
+else Console.WriteLine("третьей цифры нет");
