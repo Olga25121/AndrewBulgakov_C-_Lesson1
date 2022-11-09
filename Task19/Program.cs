21db7b9627bb0754076072ddcb4911b2e4d614d7
Olga25121 <olgajensen@mail.ru>
2022-11-09 22:06:15 +0300

    Task24

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -10,23 +10,36 @@
 Console.Write("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-void CheckPalindrom(int num)
+int num = 12345;
+if (num >= 10000 && num <= 99999)
 {
-    if (num > 9999 && num < 100000)
-    {
-        int num1 = num /10000;
-        int num5 = num % 10;
-        int num2 = num / 1000;
-        int num4 = num % 100;
-        if (num1 == num5 || num2 == num4)
-        {
-            Console.WriteLine($"Число {number} является палиндромом");
-        }
-        else
-        {
-            Console.WriteLine($"{number} - не палиндром");
-        }
-    }
-    else   Console.WriteLine("Введите пятизначное число!");
+    int num1 = num / 10000; // 1
+    int num2 = num / 1000 % 10; // 2
+    int num4 = num / 10 % 10; // 4
+    int num5 = num % 10;
+
+    if (num1 == num5 && num2 == num4) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
 }
-CheckPalindrom(number);
+else Console.WriteLine("Введено не пятизначное число");
+
+// void CheckPalindrom(int num)
+// {
+//     if (num > 9999 && num < 100000)
+//     {
+//         int num1 = num /10000;
+//         int num5 = num % 10;
+//         int num2 = num / 1000;
+//         int num4 = num % 100;
+//         if (num1 == num5 || num2 == num4)
+//         {
+//             Console.WriteLine($"Число {number} является палиндромом");
+//         }
+//         else
+//         {
+//             Console.WriteLine($"{number} - не палиндром");
+//         }
+//     }
+//     else   Console.WriteLine("Введите пятизначное число!");
+// }
+// CheckPalindrom(number);
