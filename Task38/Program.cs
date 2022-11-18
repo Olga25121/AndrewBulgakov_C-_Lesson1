a84fa04cd9a1d9a019a409b9dc3a5d76468290f4
Olga25121 <olgajensen@mail.ru>
2022-11-18 15:27:55 +0300

    Task34

---
+++ Task38/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 38: 
+// 1. Задайте массив вещественных чисел. 
+// 2. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateArrayRndDouble(int size, int min, int max)//массив для вещественных чисел
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min; //формула для нужного диапазона (double диапазон от 0-1)
+        array[i] = Math.Round(num, 1); //округление чисел до десятых
+    }
+    return array;
+}
