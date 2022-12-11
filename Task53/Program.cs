9db30e965aa670f80523357068ddeffc6c73c654
Olga25121 <olgajensen@mail.ru>
2022-12-11 17:03:17 +0300

    Task53

--- Task53/Program.cs
+++ Task53/Program.cs
@@ -2,3 +2,47 @@
 // которая поменяет местами первую и последнюю строку
 // массива.
 
+int[,]CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,]matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i; j < matrix.GetLength(1); j++)
+        {
+           matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write ("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1)-1) Console.Write($"{matrix[i, j], 5} |");
+            else Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine ("|");
+    }
+}
+//0123
+//1234
+//2345
+void ReplaceRows(int[,]matrix)
+{
+     for (int i = 0; i < matrix.GetLength(1); i++)
+     {
+        int temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+     }
+}
+int[,]array2D = CreateMatrixRndInt(3, 6, -10, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+ReplaceRows(array2D);
+PrintMatrix(array2D);
