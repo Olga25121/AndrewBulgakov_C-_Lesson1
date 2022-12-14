38473a831c4244b8ffd656edb06f081ca2ab84f1
Olga25121 <olgajensen@mail.ru>
2022-12-15 00:06:51 +0300

    Task69

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -3,3 +3,15 @@
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
 
+Console.Write("Введите натуральное число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Exponantion(int numA, int numB)
+{
+    if (numB == 0)return 1;
+    return numA * Exponantion(numA, numB-1);
+}
+int exponantion = Exponantion(numberA, numberB);
+Console.WriteLine($"Возведение числа {numberA} в степень {numberB} = {exponantion}");
