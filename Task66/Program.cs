34a64efff8ae0958918d14c9e23098a067fb1a58
Olga25121 <olgajensen@mail.ru>
2022-12-15 09:41:48 +0300

    Task68

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -3,9 +3,52 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.Write("Введите первое натуральное число: ");
+Console.Write("Введите натуральное число M: ");
 int numberM = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе натуральное число: ");
+Console.Write("Введите натуральное число N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 
-int 
+int SumNaturalElements(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m} ");
+        return m + SumNaturalElements(m + 1, n);
+    }
+    if (m > n)
+    {
+        Console.Write($"{m} ");
+        return m + SumNaturalElements(m - 1, n);
+    }
+    if (m == n)
+    {
+        Console.Write($"{m} ");
+    }
+
+}
+int sumNaturalElements = SumNaturalElements(numberM, numberN);
+Console.WriteLine($"Возведение числа {numberM} в степень {numberN} = {sumNaturalElements}");
+
+// Console.Write("Введите первое натуральное число: ");
+// int numberM = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе натуральное число: ");
+
+
+// void NaturalNumbers(int m, int n)
+// {
+//     if (m < n)
+//     {
+//          Console.Write($"{m} ");
+//         NaturalNumbers(m + 1, n);
+//     }
+//     if (m > n)
+//     {
+//         Console.Write($"{m} ");
+//         NaturalNumbers(m - 1, n);
+//     }
+//     if (m == n)
+//     {
+//         Console.Write($"{m} ");
+//     }
+// }
+// NaturalNumbers(numberM, numberN);
---
+++ Task68/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.Write("Введите два неотрицательных числа: ");
+// int numberM = Convert.ToInt32(Console.ReadLine());
+// int numberN = Convert.ToInt32(Console.ReadLine());
+
+// int Akkerman(int m, int n)
+// {
+//     if(m == 0) return n + 1;
+//     else if (m > 0 && n == 0)return Akkerman(m -1, 1);
+//     else return Akkerman(m - 1, Akkerman(m, n - 1));
+// }
+// int akkerman = Akkerman(numberM, numberN);
+// Console.WriteLine($"Возведение числа {numberM} в степень {numberN} = {akkerman}");
+Console.Write("Введите первое неотрицательное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе неотрицательное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+// AkkermanFunction(m,n);
+
+
+// // вызов функции Аккермана
+// void AkkermanFunction(int m, int n)
+// {
+//     Console.Write(Akkerman(m, n)); 
+// }
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if(m == 0) return n + 1;
+    if (m > 0 && n == 0)return Akkerman(m -1, 1);
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+int akkerman = Akkerman(numberM, numberN);
+Console.WriteLine($"Результат вычисления функции Аккермана({numberM},{numberN}) = {akkerman}");
